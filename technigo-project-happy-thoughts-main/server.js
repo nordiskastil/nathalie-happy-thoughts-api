f0d61a989c7dc398fbc219d44dbd2fcb4dcddba2
Nathalie <nathalie@nordiskastil.se>
2025-07-30 13:20:43 +0200

    Add mongoose to dependencies

--- technigo-project-happy-thoughts-main/server.js
+++ technigo-project-happy-thoughts-main/server.js
@@ -24,5 +24,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://nathalie-happy-thoughts-api.onrender.com:${10000}`);
 });
+
